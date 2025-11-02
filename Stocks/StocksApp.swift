dc351cda06087dfe408a1e6985e0b0a08de63299
Tahren <tahrenp@me.com>
2025-11-02 18:38:46 +1100

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Tahren Ponnusamy on 2/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StocksTests/StocksTests.swift
@@ -0,0 +1,16 @@
+//
+//  StocksTests.swift
+//  StocksTests
+//
+//  Created by Tahren Ponnusamy on 2/11/2025.
+//
+
+import Testing
+
+struct StocksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
